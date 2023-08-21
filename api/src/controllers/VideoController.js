befbb77dfa869780be2fe0f5586182e7b2fbd0c0
JohNNy <johnnymundi@hotmail.com>
2023-08-21 15:06:42 -0300

    SImple api finished

--- api/src/controllers/VideoController.js
+++ api/src/controllers/VideoController.js
@@ -37,4 +37,48 @@ module.exports = {
       res.status(500).json({ error: err.message });
     }
   },
+
+  async update(req, res) {
+    const { title, link } = req.body;
+
+    if (!title && !link) {
+      return res
+        .status(400)
+        .json({ error: "You must inform a new title or a new link" });
+    }
+
+    if (title) res.video.title = title;
+    if (link) res.video.link = link;
+
+    try {
+      await res.video.save();
+      return res.status(200).json({ message: "You updated successfully!" });
+    } catch (err) {
+      return res.status(500).json({ error: err.message });
+    }
+  },
+
+  async delete(req, res) {
+    try {
+      await res.video.remove();
+      return res.status(200).json({ message: "Video deleted sucessfully!" });
+    } catch (err) {
+      return res.status(500).json({ error: err.message });
+    }
+  },
+
+  // only for changing like status
+  async updateLike(req, res) {
+    res.video.liked = !res.video.liked;
+
+    try {
+      await res.video.save();
+      return res.status(200).json({
+        message: `Video ${res.video.liked ? "liked" : "unliked"} successfully!`,
+      });
+    } catch (err) {
+      return res.status(400).json({ error: err.messager });
+    }
+    next();
+  },
 };
