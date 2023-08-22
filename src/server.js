54f3d66330a4beaa359c9556fe38f2efcbbfcbb5
JohNNy <johnnymundi@hotmail.com>
2023-08-22 15:06:51 -0300

    added cors

--- src/server.js
+++ src/server.js
@@ -3,12 +3,14 @@ const express = require("express");
 const routes = require("./routes");
 const connectToDatabase = require("./database");
 require("dotenv").config({ path: path.join(__dirname, ".env") }); // necessary to connect to .env
+const cors = require("cors");
 
 connectToDatabase();
 
 const app = express();
-const port = 3333;
+const port = 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
